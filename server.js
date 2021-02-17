ee7eeed3efce4f369586b1d3a6c626b40e8366e4
Jon-graham89 <63361721+Jon-graham89@users.noreply.github.com>
2021-02-17 11:08:55 -0500

    updated port

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import express from "express";
 import bcrypt from "bcrypt-nodejs";
-import cors from 'cors';
-import knex from 'knex';
-import handleRegister from './controllers/register.js'
-import signinHandler from './controllers/signin.js'
-import imageHandler from './controllers/image.js'
-import profileHandler from './controllers/profile.js'
+import cors from "cors";
+import knex from "knex";
+import handleRegister from "./controllers/register.js";
+import signinHandler from "./controllers/signin.js";
+import imageHandler from "./controllers/image.js";
+import profileHandler from "./controllers/profile.js";
 // import Clarifai from 'clarifai'
 
 // const app = new Clarifai.App({
@@ -13,38 +13,40 @@ import profileHandler from './controllers/profile.js'
 // });
 
 const db = knex({
-  client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '5EuroBit',
-    database : 'facerecognition'
-  }
+	client: "pg",
+	connection: {
+		host: "127.0.0.1",
+		user: "postgres",
+		password: "5EuroBit",
+		database: "facerecognition",
+	},
 });
-
-
-
 
 const app = express();
 
-
-
-
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.get("/", (req, res) => {
-	res.send('success');
+	res.send("success");
 });
 
-app.post("/signin", (req,res) => {signinHandler(req, res, db, bcrypt)});
+app.post("/signin", (req, res) => {
+	signinHandler(req, res, db, bcrypt);
+});
 
-app.post("/register", (req, res) => {handleRegister(req, res, db, bcrypt)});
+app.post("/register", (req, res) => {
+	handleRegister(req, res, db, bcrypt);
+});
 
-app.get("/profile/:id", (req, res) => {profileHandler(req, res, db)})
+app.get("/profile/:id", (req, res) => {
+	profileHandler(req, res, db);
+});
 
-app.put("/image", (req, res) => {imageHandler(req, res, db)});
+app.put("/image", (req, res) => {
+	imageHandler(req, res, db);
+});
 
-app.listen(3000, () => {
-	console.log("app is running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is running on port ${process.env.PORT} `);
 });
